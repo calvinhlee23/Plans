95646e714b4b3d89b839a5d1954b8e960c208fca
calvin <calvinhlee23@gmail.com>
2017-05-09 17:44:56 -0700

    bug fixes

--- db/migrate/20170421215437_add_creator_to_events.rb
+++ db/migrate/20170421215437_add_creator_to_events.rb
@@ -1,6 +1,6 @@
 class AddCreatorToEvents < ActiveRecord::Migration
   def change
-    add_index :events, :creator_id
     add_column :events, :creator_id, :integer, null: false
+    add_index :events, :creator_id
   end
 end
